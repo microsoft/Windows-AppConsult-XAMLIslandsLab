35ce6d2652f3ebe7f47ec92c4e97fb4f0ed2b664
Sebastien Bovo <sebastienbovo@outlook.com>
2019-01-11 10:40:02 +0100

    Contoso Expense!

--- Lab/Exercise1/01-Start/ContosoExpenses/ContosoExpenses/ExpenseDetail.xaml.cs
+++ Lab/Exercise1/01-Start/ContosoExpenses/ContosoExpenses/ExpenseDetail.xaml.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// ******************************************************************
+
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+
+// ******************************************************************
+
+using System;
 using System.Linq;
 using System.Windows;
 using ContosoExpenses.Models;
@@ -23,6 +37,7 @@ namespace ContosoExpenses
             txtDescription.Text = SelectedExpense.Description;
             txtLocation.Text = SelectedExpense.Address;
             txtAmount.Text = SelectedExpense.Cost.ToString();
+            Chart.Height = (SelectedExpense.Cost * 400) / 1000;
         }
     }
 }
