a9321750c6f113fcf7a66d65252a6a2e77f573bd
Matteo Pagani <matteo.pagani@microsoft.com>
2019-04-08 22:46:24 +0200

    Upgraded to Toolkit 6 beta

---
+++ Lab/Exercise5/02-End/ContosoExpenses/ContosoExpenses/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Toolkit.Win32.UI.XamlHost;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoExpenses
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (var xamlApp = new XamlApplication())
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
